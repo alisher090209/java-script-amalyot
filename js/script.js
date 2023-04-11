2dc44c72bb823a1d5c786de4fbb8c389e58d2abe
Your Name <you@example.com>
2023-04-11 13:29:44 +0300

    alisher

--- js/script.js
+++ js/script.js
@@ -6,18 +6,22 @@ function showMyslider (){
  if(result.innerText = this.value <=30){
     body.classList.add("body1");
     result.innerText = this.value;
+    result.style.color = "#fff";
  }
  else if(result.innerText = this.value <=60){
     body.classList.add("body2");
     result.innerText = this.value;
+    result.style.color = "red";
  }
  else if(result.innerText = this.value <=90){
     body.classList.add("body3");
     result.innerText = this.value;
+    result.style.color = "green";
  }
  else if(result.innerText = this.value <=100){
     body.classList.add("body4");
     result.innerText = this.value;
+    result.style.color = "blue";
  }
 }
 
