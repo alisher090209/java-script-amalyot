6fe6364e26affb7540ebf6b213dc17443d7ae0e1
Your Name <you@example.com>
2023-04-11 08:53:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict"
+mySilder.oninput = showMyslider;
+
+function showMyslider (){
+ result.innerText = this.value; 
+ if(result.innerText = this.value <=30){
+    body.classList.add("body1");
+    result.innerText = this.value;
+ }
+ else if(result.innerText = this.value <=60){
+    body.classList.add("body2");
+    result.innerText = this.value;
+ }
+ else if(result.innerText = this.value <=90){
+    body.classList.add("body3");
+    result.innerText = this.value;
+ }
+ else if(result.innerText = this.value <=100){
+    body.classList.add("body4");
+    result.innerText = this.value;
+ }
+}
+
+
+
+const body = document.querySelector(".body"),
+      card = document.querySelector(".card");
+      console.log(card);
